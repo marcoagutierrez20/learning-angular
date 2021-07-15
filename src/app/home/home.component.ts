c2baf2311a1818085b824ff0a688f005c5c3420b
Marco <mgutie60@mail.depaul.edu>
2021-07-15 16:22:45 -0500

    Added content to the home page. Created a card that demonstarts string interpolation and property/event binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  clickCount: number = 0; //clickCount is of type number and initalized to 0
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  countClick() { //countClick function will add 1 onto clickCount
+    this.clickCount += 1;
+  }
 }
